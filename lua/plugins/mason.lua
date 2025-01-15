c4d36bba05337fd4bb35ff1734da70c505c3fa9a
3D_Daxter <daxter.von@proton.me>
2025-01-15 17:27:45 +0100

    added codeactions

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -66,6 +66,8 @@ return {
             lspconfig.jinja_lsp.setup({})
 
             vim.keymap.set('n','K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n','<leader>gd', vim.lsp.buf.definition, {})
+            vim.keymap.set({'n'}, '<leader>ca', vim.lsp.buf.code_action, {})
         end
     }
 }
